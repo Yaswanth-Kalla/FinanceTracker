755e43e6b7f9f22fb8d66499a600da05b3f8f92e
Yaswanth-Kalla <155803044+Yaswanth-Kalla@users.noreply.github.com>
2024-12-13 14:26:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/callback",
+      callbackURL: "https://expensetracker-lta3.onrender.com/auth/google/callback",
     },
     async function (accessToken, refreshToken, profile, cb) {
       try {
